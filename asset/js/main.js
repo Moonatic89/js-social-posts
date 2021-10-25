aef8b5c213f07cc66ad3e16cdb1ba0e34c1ca7bb
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-25 17:07:52 +0200

    compiling template post

---
+++ asset/js/main.js
@@ -0,0 +1,41 @@
+// Creare una pagina che elenchi una lista di post socials a partire da un array di oggetti.
+// Gli attributi minimi del modello di un post: id, contenuto, immagine,
+//     autore(nome, avatar), numero di likes, data creazione.
+// Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
+
+/*
+//this should be an array of posts
+POST TEMPLATE
+const post {
+id: id,
+bodypost: "lorem ipsum",
+image: url,
+user: username,
+user: userimg,
+likes: likes#,
+date: date
+
+}
+
+//A second array should contain only posts id and a like bool
+
+*/
+
+//This is only for readability
+const loremIps = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, a. Similique rem ullam laudantium nesciunt!";
+const posts = [
+    {
+        id: 0,
+        bodypost: loremIps,
+        image: url,
+        user: username,
+        user: userimg,
+        likes: likes,
+        date: date
+    }
+]
+
+
+
+
+
