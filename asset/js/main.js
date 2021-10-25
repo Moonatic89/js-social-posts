29789e115a130042aa79d70f1afa7533c0882eb2
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-25 18:46:28 +0200

    working on template card

--- asset/js/main.js
+++ asset/js/main.js
@@ -15,16 +15,36 @@ const posts = [
     {
         id: 0,
         bodypost: loremIps,
-        image: "https://i.picsum.photos/id/873/200/300",
-        user: "Andrea Tommasei",
-        user: "https://i.picsum.photos/id/873/200/300",
+        image: "https://picsum.photos/200/300",
+        userName: "Andrea Tommasei",
+        userImage: "https://picsum.photos/200/305",
         likes: 12,
         date: "4 months ago"
     }
 ]
+// DEBUG POST CREATION###
+
+for (let i = 0; i < posts.length; i++) {
+    //    const element = posts[i];
+
+    const userImg = document.querySelector(".userImg");
+    const userName = document.querySelector(".userName");
+    const userDate = document.querySelector(".postDate");
+    const bodyText = document.querySelector(".postText");
+    const image = document.querySelector(".postImg");
+    const likeBtn = document.querySelector(".postLikeBtn");
+    const likeCnt = document.querySelector(".postLikes");
+
+    userImg.innerHTML = `<img src="${posts[i].userImage}" alt="User Image">`;
+    userName.innerHTML = `${posts[i].userName}`;
+    userDate.innerHTML = `${posts[i].date}`
+    bodyText.innerHTML = `<p>${posts[i].bodypost}</p>`
+    image.innerHTML = `<img src = "${posts[i].image}" alt = "Post Image" > `
+    //likeBtn.innerHTML = `${posts[i].user}`;
+    likeCnt.innerHTML = `${posts[i].likes} likes this`
 
 
-
+}
 
 
 
