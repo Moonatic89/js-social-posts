393b13daf7a716fd8f2308adfaf63e6ac8ceea02
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-27 07:35:19 +0200

    Finished

--- asset/js/main.js
+++ asset/js/main.js
@@ -11,41 +11,115 @@
 
 //This is only for readability
 const loremIps = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, a. Similique rem ullam laudantium nesciunt!";
+
 const posts = [
     {
         id: 0,
         bodypost: loremIps,
-        image: "https://picsum.photos/200/300",
+        image: "https://picsum.photos/800/300",
         userName: "Andrea Tommasei",
         userImage: "https://picsum.photos/200/305",
         likes: 12,
+        likeBool: false,
+        likeIcon: "Like",
         date: "4 months ago"
+    }, {
+        id: 0,
+        bodypost: loremIps,
+        image: "https://picsum.photos/810/300",
+        userName: "Fabio De Luigi",
+        userImage: "https://picsum.photos/202/305",
+        likes: 15,
+        likeBool: false,
+        likeIcon: "Like",
+        date: "6 months ago"
+    }, {
+        id: 0,
+        bodypost: loremIps,
+        image: "https://picsum.photos/800/310",
+        userName: "Franco Mangiasabbia",
+        userImage: "https://picsum.photos/206/305",
+        likes: 1,
+        likeBool: false,
+        likeIcon: "Like",
+        date: "1 hour ago"
     }
 ]
+
 // DEBUG POST CREATION###
 
 for (let i = 0; i < posts.length; i++) {
-    //    const element = posts[i];
+    const post = posts[i];
 
-    const userImg = document.querySelector(".userImg");
-    const userName = document.querySelector(".userName");
-    const userDate = document.querySelector(".postDate");
-    const bodyText = document.querySelector(".postText");
-    const image = document.querySelector(".postImg");
-    const likeBtn = document.querySelector(".postLikeBtn");
-    const likeCnt = document.querySelector(".postLikes");
 
-    userImg.innerHTML = `<img src="${posts[i].userImage}" alt="User Image">`;
-    userName.innerHTML = `${posts[i].userName}`;
-    userDate.innerHTML = `${posts[i].date}`
-    bodyText.innerHTML = `<p>${posts[i].bodypost}</p>`
-    image.innerHTML = `<img src = "${posts[i].image}" alt = "Post Image" > `
-    //likeBtn.innerHTML = `${posts[i].user}`;
-    likeCnt.innerHTML = `${posts[i].likes} likes this`
+    const el = createStructure(post);
+    console.log(el);
+    document.querySelector(".posts").insertAdjacentHTML("beforeend", el);
+
+
+    // const bodyText = document.getElementById(`postText${idNum}`);
+    // const image = document.getElementById(`postImg${idNum}`);
+    // const likeBtn = document.getElementById(`postLikeBtn${idNum}`);
+    // const likeCnt = document.getElementById(`postLikes${idNum}`);
+
+    // bodyText.innerHTML = `<p>${post.bodypost}</p>`
+    // image.innerHTML = ` `
+    // likeBtn.innerHTML = ``;
+    // likeCnt.innerHTML = ``
 
 
 }
 
+function createStructure(post) {
+    return `
+                <div class="post">
 
+                    <div class="user">
+
+                        <div  class="userImg">
+                            <img src="${post.userImage}" alt="User Image">
+                        </div>
+
+                        <div class="userSide">
+
+                            <div class="userName">
+                                ${post.userName}
+                            </div>
+
+                            <div class="postDate">
+                                ${post.date}
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="postBody">
+
+                        <div  class="postText">
+                            ${post.bodypost}
+                        </div>
+
+                        <div class="postImg">
+                            <img src = "${post.image}" alt = "Post Image" >
+                        </div>
+
+                        <div class="postSocial">
+
+                            <div  class="postLikeBtn">
+                                <i class="fas fa-thumbs-up"></i>Like
+                            </div>
+
+                            <div class="postLikes">
+                                ${post.likes} likes this
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>`
+
+}
 
 
